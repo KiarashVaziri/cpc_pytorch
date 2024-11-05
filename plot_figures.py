8f1860ffee88ad02e0cf702ac5feb8fa074a8145
Kiarash Vaziri Goodarzi <vazirigo@endeavour1.hpc.usc.edu>
2024-11-04 19:52:28 -0800

    added new figures

--- plot_figures.py
+++ plot_figures.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define your variables
-ar_type = 'gru'
-use_ldm_params = 0
+ar_type = 'ldm'
+use_ldm_params = 1
 k = 4
 loss_flag = 'mse'
-rnd_seeds = [22, 23, 24, 25]
-pickle_files = [f"metrics/metrics_{ar_type}_ldmfcst{use_ldm_params}_{k}_rnd{rnd_seed}_mse_dtchTrue" for rnd_seed in rnd_seeds]
+rnd_seeds = [21, 22, 23, 24, 25]
+pickle_files = [f"metrics/metrics_{ar_type}_ldmfcst{use_ldm_params}_{k}_rnd{rnd_seed}_mse_dtchTrue_lnrw" for rnd_seed in rnd_seeds]
 
 # Initialize list to store test_acc values and dictionary for metrics
 test_accs = []
@@ -70,7 +70,7 @@ plt.fill_between(range(len(avg_metrics['epoch_acc_validation'])), min_metrics['e
 plt.xlabel('Epochs'); plt.ylabel('Accuracy'); plt.title(f'avg test acc: %{100*avg_test_acc:3.3f}'); plt.legend()
 
 # Adjust layout and save the figure
-file_name = f"figs/epochs_{ar_type}_ldmfcst{use_ldm_params}_k{k}_{loss_flag}_dtchTrue.png"
+file_name = f"figs/epochs_{ar_type}_ldmfcst{use_ldm_params}_k{k}_{loss_flag}_dtchTrue_linearw.png"
 plt.tight_layout()
 plt.savefig(file_name)
 plt.show()
